e7c85ecab10e7acaa729d9956fb59c8a91ed7feb
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-04-13 17:36:54 +0100

    Refactor config to file

--- display.py
+++ display.py
@@ -1,5 +1,8 @@
 import json
 import os
+import ConfigParser
+import argparse
+
 import tweepy
 import festival
 import pyglet
@@ -58,19 +61,43 @@ class Twitter2Speech(tweepy.StreamListener):
         return True
 
 
-def main():
-    consumer_key = ""
-    consumer_secret = ""
-    access_token = ""
-    access_token_secret = ""
-    l = Twitter2Speech("pyglet")
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    hash = "programming"
-    print "Showing all new tweets for #" + hash + ":"
-    stream = tweepy.Stream(auth, l)
-    stream.filter(track=[hash])
-
+def read_config(path):
+    cp = ConfigParser.ConfigParser()
+    cp.read(path)
+    ck = cp.get("CONSUMER", "key")
+    cs = cp.get("CONSUMER", "secret")
+    at = cp.get("ACCESS", "token")
+    ase = cp.get("ACCESS", "secret")
+    data = (ck, cs, at, ase)
+    mode = cp.get("GENERAL", "mode")
+    hash = cp.get("GENERAL", "hashtag")
+    return data, mode, hash
 
 if __name__ == "__main__":
+    def parse_cmd():
+        """
+        Parses command line and returns path string.
+        Expected `python -f input.txt`.
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-f', '--file',
+                            dest='file',
+                            required=True,
+                            help='Path of file to analyse')
+        args = parser.parse_args()
+        return (args.file)
+
+    def main():
+        path = parse_cmd()
+        p, m, hash = read_config(path)
+        consumer_key = p[0]
+        consumer_secret = p[1]
+        access_token = p[2]
+        access_token_secret = p[3]
+        l = Twitter2Speech(m)
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        print "Showing all new tweets for #" + hash + ":"
+        stream = tweepy.Stream(auth, l)
+        stream.filter(track=[hash])
     main()
